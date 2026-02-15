1174334f155bcb791e86177cab596fa147bd56f9
Naveen kumar <98445817+Naveen2334@users.noreply.github.com>
2026-02-15 14:01:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Chocolate Distribution Problem/chocolate-distribution-problem.java
@@ -0,0 +1,25 @@
+// User function Template for Java
+
+class Solution {
+    public int findMinDiff(ArrayList<Integer> arr, int m) {
+        
+        // your code here
+        int n = arr.size();
+        if(m==0||n==0){
+            return 0;
+            
+        }
+        if(n<m){
+            return -1;
+            
+        }
+        Collections.sort(arr);
+        int minDiff = Integer.MAX_VALUE;
+        for(int i=0;i<=n-m;i++){
+            int diff = arr.get(i+m-1)-arr.get(i);
+            minDiff = Math.min(minDiff,diff);
+        }
+        return minDiff;
+        
+    }
+}
